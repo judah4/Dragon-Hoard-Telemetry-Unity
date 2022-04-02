95532e8be511d7372c219c8b6549ac96c9ccd572
Judah Perez <judah@cookiedragon.games>
2022-04-02 11:52:05 -0700

    Starting unity library

---
+++ Packages/opentelemetry-unity/Scripts/Api/Tracing/TelemetryTracer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OpenTelemetry.Unity
+{
+
+
+    public class TelemetryTracer
+    {
+        public string Name { get; }
+    }
+
+}
---
+++ Packages/opentelemetry-unity/Scripts/Api/Tracing/TracerProvider.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OpenTelemetry.Unity
+{
+
+    public class TracerProvider
+    {
+        public const string Version = "0.1.0";
+        public const string SchemaVersion = "1.10.0";
+
+        public TelemetryTracer GetTracer(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                name = "OpenTelemetryUnity";
+            }
+
+            var tracer = new TelemetryTracer()
+            {
+                Name = name,
+            }
+        }
+    }
+
+}
